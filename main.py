ad437abf05a45295c20ba74c9206446082853d83
Kakaru_0 <81516330+bvnm0121@users.noreply.github.com>
2023-03-14 21:52:03 +0900

    [commit]

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def main():
-    return 'hello Docker!'
+    return 'hello Docker! Wow'
 
 if __name__=='__main__':
         uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
